3e1e8562d139c0c36074dab0fef90dd98ffddd1c
Vladimir Guguiev <wizardzloy@gmail.com>
2015-12-29 17:35:55 +0100

    export FocusableTreeUtils

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = {
   FocusManager: require('./dist/FocusManager')['default'],
   Focusable: require('./dist/Focusable')['default'],
   FocusableContainer: require('./dist/FocusableContainer')['default'],
-  FocusableTreeUtils: require('./dist/utils/FocusableTreeUtils')['default']
+  FocusableTreeUtils: require('./dist/utils/FocusableTreeUtils')
 };
