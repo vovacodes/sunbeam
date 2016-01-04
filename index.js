e293f9dc5f435a75523e14ef1353cd31de5a83e7
Vladimir Guguiev <wizardzloy@gmail.com>
2016-01-04 11:38:57 +0100

    export strategies

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ module.exports = {
   FocusManager: require('./dist/FocusManager')['default'],
   Focusable: require('./dist/Focusable')['default'],
   FocusableContainer: require('./dist/FocusableContainer')['default'],
-  FocusableTreeUtils: require('./dist/utils/FocusableTreeUtils')
+  FocusableTreeUtils: require('./dist/utils/FocusableTreeUtils'),
+  Strategies: {
+    horizontalIndexBasedFocusStrategy: require('./dist/strategies/horizontalIndexBasedFocusStrategy'),
+    verticalIndexBasedFocusStrategy: require('./dist/strategies/verticalIndexBasedFocusStrategy')
+  }
 };
